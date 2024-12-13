77fccde75b4ed610309a1cc0bc5f85a7e6318447
Mario Medina <mariomedinam@gmail.com>
2024-12-13 16:55:10 -0500

    Add URL paths for SeleccionarExpedienteSecuencia and DownloadFormatoDevengadoController

--- project_mpp/app_siaf/urls.py
+++ project_mpp/app_siaf/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import MaestroDocumentoView, PersonaView, ProveedorSIGAView, SeleccionarExpedienteFase
+from .views import MaestroDocumentoView, PersonaView, ProveedorSIGAView, SeleccionarExpedienteFase, SeleccionarExpedienteSecuencia, DownloadFormatoDevengadoController
 
 urlpatterns = [
     path("maestro-documento/", MaestroDocumentoView.as_view()),
     path("persona/", PersonaView.as_view()),
     path("proveedor-siga/", ProveedorSIGAView.as_view()),
     path("seleccionar-expediente-fase", SeleccionarExpedienteFase.as_view()),
+    path("seleccionar-expediente-secuencia", SeleccionarExpedienteSecuencia.as_view()),
+    path("download-formato-devengado", DownloadFormatoDevengadoController),
 ]
