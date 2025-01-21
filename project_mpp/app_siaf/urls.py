59dccd775606b8be4dd9f9e44c3e10a1daaa45d7
Mario Medina <mariomedinam@gmail.com>
2025-01-21 11:35:35 -0500

    Add URL pattern for ProcesoActualizarRegistroView to enable record updates

--- project_mpp/app_siaf/urls.py
+++ project_mpp/app_siaf/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MaestroDocumentoView, PersonaView, ProveedorSIGAView, SeleccionarExpedienteFase, SeleccionarExpedienteSecuencia, DownloadFormatoDevengadoController
+from .views import MaestroDocumentoView, PersonaView, ProveedorSIGAView, SeleccionarExpedienteFase, SeleccionarExpedienteSecuencia, DownloadFormatoDevengadoController, ProcesoActualizarRegistroView
 
 urlpatterns = [
     path("maestro-documento/", MaestroDocumentoView.as_view()),
@@ -8,4 +8,5 @@ urlpatterns = [
     path("seleccionar-expediente-fase", SeleccionarExpedienteFase.as_view()),
     path("seleccionar-expediente-secuencia", SeleccionarExpedienteSecuencia.as_view()),
     path("download-formato-devengado", DownloadFormatoDevengadoController),
+    path("proceso-actualizar-registro", ProcesoActualizarRegistroView.as_view()),
 ]
