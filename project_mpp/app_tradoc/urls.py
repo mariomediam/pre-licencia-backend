31c0bcda40eee344823da3fc1d3bb440c1b62354
Mario Medina <mariomedinam@gmail.com>
2025-11-06 12:46:21 -0500

    Agregar la ruta para el controlador SeleccDocumController en urls.py, permitiendo el acceso a la funcionalidad de selección de documentos.

--- project_mpp/app_tradoc/urls.py
+++ project_mpp/app_tradoc/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import (ExpedientesController,)
+from .views import (ExpedientesController, SeleccDocumController)
 
 urlpatterns = [
     path('expedientes/<str:numero>/<str:anio>', ExpedientesController.as_view()),
+    path('selecc-docum', SeleccDocumController.as_view()),
 ]
