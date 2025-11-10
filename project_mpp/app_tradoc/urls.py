0a555446bfc7902de33a246fad71f408c98b071f
Mario Medina <mariomedinam@gmail.com>
2025-11-10 10:41:30 -0500

    Agregar rutas para los controladores VerArbolController y VerUltimaRamaArbolController en urls.py, permitiendo el acceso a nuevas funcionalidades de visualización de la estructura de documentos.

--- project_mpp/app_tradoc/urls.py
+++ project_mpp/app_tradoc/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import (ExpedientesController, SeleccDocumController)
+from .views import (ExpedientesController, SeleccDocumController, VerArbolController, VerUltimaRamaArbolController)
 
 urlpatterns = [
     path('expedientes/<str:numero>/<str:anio>', ExpedientesController.as_view()),
     path('selecc-docum', SeleccDocumController.as_view()),
+    path('ver-arbol', VerArbolController.as_view()),
+    path('ver-ultima-rama-arbol', VerUltimaRamaArbolController.as_view()),
 ]
