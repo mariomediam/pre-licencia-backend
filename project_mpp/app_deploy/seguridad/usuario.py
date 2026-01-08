8eecbc10086b728b528579b29e7c81498229edfc
Mario Medina <mariomedinam@gmail.com>
2026-01-07 22:45:01 -0500

    Agregar la función get_user_by_login en usuario.py para permitir la búsqueda de usuarios por su login, mejorando la gestión de autenticación.

--- project_mpp/app_deploy/seguridad/usuario.py
+++ project_mpp/app_deploy/seguridad/usuario.py
@@ -4,4 +4,14 @@ from app_deploy.general.ejecutar import dictfetchall
 def login(usuario, password, sistema='02'):
     with connection.cursor() as cursor:        
         cursor.execute('EXEC GENERAL.dbo.S07ValidarUsuario2 %s, %s, %s', [usuario, password, sistema])        
-        return dictfetchall(cursor)        
+        return dictfetchall(cursor)    
+
+def get_user_by_login(login):
+    with connection.cursor() as cursor:
+        cursor.execute('EXEC GENERAL.dbo.S07BuscarUsuario %s, %s', ['C', login])
+        result = dictfetchall(cursor)
+        if len(result) > 0:
+            return result[0]
+        else:
+            return None
+
